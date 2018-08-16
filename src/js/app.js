b22cef53a072370fc32bba07482c76523b52c869
Noelia Sabando <lolia_15@hotmail.com>
2018-08-15 21:46:32 -0400

    modal ok

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,17 @@
+$('#exampleModalCenter').on('show.bs.modal', function (event) {
+  var card = $(event.relatedTarget) 
+  var imagenUrl = card.data("img") 
+  var likesUrl= card.data("likes")
+  var userUrl= card.data("user")
+  var modal = $(this)
+  modal.find(".modal-imagen").attr("src", imagenUrl)
+  modal.find(".modal-likes").text("Likes: " + likesUrl)
+  modal.find(".modal-user").text("Usuario: " + userUrl)
+})
+
+
 const inputText= document.querySelector("input");
 const listaImagenes= document.getElementById("listaImagenes");
-
 
 
 inputText.addEventListener("keypress", (event)=> {
@@ -9,7 +20,6 @@ inputText.addEventListener("keypress", (event)=> {
     let imagenes=inputText.value;
     console.log(imagenes)
     inputText.value= ""; 
-
     fetch(`https://pixabay.com/api/?key=9828833-9a15a3dc938016b93df55afe1&q=${imagenes}&image_type=photo`)
       .then(response => response.json()) 
       .then(datos =>{ 
@@ -22,11 +32,11 @@ inputText.addEventListener("keypress", (event)=> {
 const renderInfo = datos => {
   listaImagenes.innerHTML = "";
   datos.hits.forEach((imagen) => {
-    listaImagenes.innerHTML += `<div class="imagenes">
-    <img src=${imagen.webformatURL} id="imagen">
+    listaImagenes.innerHTML += `<div class="imagenes" data-toggle="modal" data-target="#exampleModalCenter" data-img=${imagen.webformatURL} data-likes=${imagen.likes} data-user=${imagen.user}>
+    <img src=${imagen.webformatURL} class="imagen">
     <h5>Título: Pixabay</h5>
-    <p id="likes">Likes: ${imagen.likes}</p>
-    <p id="usuario">Usuario: ${imagen.user}</p>
+    <h6 class="likes">Likes: ${imagen.likes}</h6>
+    <p class="usuario">Usuario: ${imagen.user}</p>
     </div>`    
   })
 } 
